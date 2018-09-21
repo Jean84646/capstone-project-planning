5b0533c29407566fbbe3dc2bab85a6447b0a73db
Jean Jia <jean84646@gmail.com>
2018-09-21 14:40:02 -0700

    Working on App component

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import bg from './../img/bg1.jpg';
+
+import { Switch, Route } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: [{}]
+    };
+  }
+
+  render(){
+  return (
+    <div>
+      <style jsx>{`
+        body {
+          font-family: monospace;
+          background-image: url(${bg});
+          background-size: cover;
+          background-repeat: no-repeat;
+        }
+        `}</style>
+      <div className="container">
+        <Header/>
+      </div>
+    </div>
+  );
+}
+}
+
+export default App;
