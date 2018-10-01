2e668a4ba72978486fb4dda81cc612fef71b54d4
Jean Jia <jean84646@gmail.com>
2018-10-01 02:48:46 -0700

    commit weekend work all together.

---
+++ src/components/StepByStep.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function StepByStep() {
+  return (
+    <div>
+      <ul>
+        <li>Step 1: check pulse</li>
+        <li>Step 2: 30 chest compressions</li>
+        <li>Step 3: 2 breaths</li>
+      </ul>
+    </div>
+  );
+}
+
+export default StepByStep;
